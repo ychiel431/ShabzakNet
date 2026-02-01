f6d56c9454f7385f7f5b5ae7219f03b1fac384d0
Yechiel Hisherik <y0583236450@gmail.com>
2026-02-01 11:02:28 +0200

    Align main.py with new database schema and IP

--- server/database.py
+++ server/database.py
@@ -3,7 +3,7 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from pymongo import MongoClient
 
 # --- ניהול כתובת השרת ---
-# עדכון לכתובת ה-IP הקבועה של השרת ב-AWS
+# כתובת ה-IP הקבועה של השרת ב-AWS
 SERVER_IP = "98.83.47.167"
 PORT = 8080
 CURRENT_BASE_URL = os.getenv("BASE_URL", f"http://{SERVER_IP}:{PORT}")
@@ -14,22 +14,26 @@ STATIC_DIR = os.path.join(BASE_DIR, "static")
 QR_DIR = os.path.join(STATIC_DIR, "qrcodes")
 PHOTO_DIR = os.path.join(STATIC_DIR, "photos")
 
-# וודא שהתיקיות קיימות פיזית בשרת
+# וודא שהתיקיות קיימות פיזית
 for d in [QR_DIR, PHOTO_DIR]:
     os.makedirs(d, exist_ok=True)
 
 # --- חיבור למסד הנתונים ---
-# ברירת מחדל לחיבור בתוך Docker
+# הגדרת הכתובת לחיבור בתוך Docker
 MONGO_URI = os.getenv("MONGO_URI", "mongodb://mongo:27017")
 
-# חיבור אסינכרוני (Motor) - עבור FastAPI
+# חיבור אסינכרוני
 client = AsyncIOMotorClient(MONGO_URI)
 database = client.shabzaknet_db
 
-# חיבור סינכרוני (PyMongo) - עבור סקריפטים וגיבויים
+# --- תיקון השמות עבור main.py ---
+# אנחנו מגדירים את 'db' ככינוי ל-'database' כדי שהייבוא במיין יעבוד
+db = database 
+
+# חיבור סינכרוני (לשימוש ב-db_sync)
 client_sync = MongoClient(MONGO_URI)
 db_sync = client_sync.shabzaknet_db
 
-# קיצורי דרך לקולקשנים (מתוקן לשימוש ב-database האסינכרוני)
+# קיצורי דרך לקולקשנים (לשימוש בתוך הפונקציות)
 soldiers_col = database.soldiers
 vehicles_col = database.vehicles
